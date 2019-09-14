923cbd8dc9448df0e70239259128517456bdbdd8
Shin-Yi <shinyi@gmail.com>
2019-09-14 03:58:38 -0400

    Added Backend
    Backend with Node and some JS functions for frontender to use

---
+++ backend/test.js
@@ -0,0 +1,49 @@
+var express = require('express')
+var app = express()
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+var orders = {}
+var users = {}
+
+var order_id = 0
+
+app.get('/', function (req, res) {
+    res.sendFile('./index.html', {root:__dirname})
+})
+
+app.post('/neworder', function (req, res) {    
+    orders[order_id] = req.body    
+    res.send("Your order ID is: " + order_id)
+    order_id += 1
+})
+
+app.post('/checkorder', function (req, res) {
+    res.send("You requested to check order ID: " + req.body['orderid'] + ", which is " + JSON.stringify(orders[req.body['orderid']]))
+})
+
+app.post('/acceptorder', function (req, res) {
+    orders[req.body['orderid']]['status'] = "ACCEPTED"
+    orders[req.body['orderid']]['courierid'] = req.body['courierid']
+    res.send("You have accepted order " + JSON.stringify(orders[req.body['orderid']]))
+})
+
+app.post('/arrived', function (req, res) {
+    if (orders[req.body['orderid']]['courierid'] == req.body['courierid']) {
+        orders[req.body['orderid']]['status'] = "ARRIVED"
+        res.send('Your order ' + JSON.stringify(orders[req.body['orderid']]) + ' has arrived.');
+    } else {
+      res.send('You are not authorized to make this change')
+    }
+})
+
+app.post('/delivered', function (req, res) {
+    if (orders[req.body['orderid']]['userid'] == req.body['userid']) {
+        orders[req.body['orderid']]['status'] = "DELIVERED"
+        res.send('Your are confirming that your order ' + JSON.stringify(orders[req.body['orderid']]) + ' has been delivered.');
+    } else {
+      res.send('You are not authorized to make this change')
+    }
+})
+
+app.listen(80)
