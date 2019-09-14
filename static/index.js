02295bd7e5f15e10c911a8e0596e108d7c55984a
Jack Duvall <jediguy9@gmail.com>
2019-09-13 23:09:42 -0400

    Woops forgot to commit

--- static/index.js
+++ static/index.js
@@ -1,1 +1,14 @@
-require("html-loader!./html/test.html");
+require("html-loader!./html/index.html");
+import './scss/index.scss';
+import $ from 'jquery';
+
+window.onload = function() {
+  console.log("loaded!");
+}
+$(".header-righticon").click(function() {
+  if($(".header-menuexpand").css("display") == "none") {
+    $(".header-menuexpand").css("display", "flex");
+  } else {
+    $(".header-menuexpand").css("display", "none");
+  }
+});
