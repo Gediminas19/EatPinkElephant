851cfa9b61871dd8cf38737cda573748c6fa9dd3
Shin-Yi <shinyi@gmail.com>
2019-09-14 20:54:26 -0400

    Fixed port
    Made it hostable without port changing

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-const PORT = 8000;
+const PORT = 80;
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
